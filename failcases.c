eebf4524d76e9e3d7058615fdd004b85aaba9980
ConnorBrereton <breretonconnor@gmail.com>
2018-12-21 19:19:12 +0000

    Changed failure cases to print to stderr

--- failcases.c
+++ failcases.c
@@ -17,53 +17,53 @@ void error(int error, char *info)
 	switch (error)
 	{
 		case NO_ARGS:
-			printf("USAGE: monty file\n");
+			fprintf(stderr, "USAGE: monty file\n");
 			break;
 		case NO_FILE:
-			printf("Error: Can't open file %s\n", info);
+			fprintf(stderr, "Error: Can't open file %s\n", info);
 			break;
 		case NO_DATA:
-			printf("L%d: usage: push integer\n", command->line_number);
+			fprintf(stderr, "L%d: usage: push integer\n", command->line_number);
 			break;
 		case PINT_FAIL:
-			printf("L%d: can't pint, stack empty\n", command->line_number);
+			fprintf(stderr, "L%d: can't pint, stack empty\n", command->line_number);
 			break;
 		case POP_FAIL:
-			printf("L%d: can't pop an empty stack\n", command->line_number);
+			fprintf(stderr, "L%d: can't pop an empty stack\n", command->line_number);
 			break;
 		case NO_TWO_NODES:
-			printf("L%d: can't swap, stack too short\n", command->line_number);
+			fprintf(stderr, "L%d: can't swap, stack too short\n", command->line_number);
 			break;
 		case CANT_ADD:
-			printf("L%d: can't add, stack too short\n", command->line_number);
+			fprintf(stderr, "L%d: can't add, stack too short\n", command->line_number);
 			break;
 		case CANT_SUB:
-			printf("L%d: can't sub, stack too short\n", command->line_number);
+			fprintf(stderr, "L%d: can't sub, stack too short\n", command->line_number);
 			break;
 		case CANT_DIV:
-			printf("L%d: can't div, stack too short\n", command->line_number);
+			fprintf(stderr, "L%d: can't div, stack too short\n", command->line_number);
 			break;
 		case CANT_MUL:
-			printf("L%d: can't mul, stack too short\n", command->line_number);
+			fprintf(stderr, "L%d: can't mul, stack too short\n", command->line_number);
 			break;
 		case CANT_MOD:
-			printf("L%d: can't mod, stack too short\n", command->line_number);
+			fprintf(stderr, "L%d: can't mod, stack too short\n", command->line_number);
 			break;
 		case PCHAR_FAIL:
-			printf("L%d: can't pchar, stack empty\n", command->line_number);
+			fprintf(stderr, "L%d: can't pchar, stack empty\n", command->line_number);
 			break;
 		case UNDEFINED:
 		case UNDEFINEDD:
-			printf("L%d: division by zero\n", command->line_number);
+			fprintf(stderr, "L%d: division by zero\n", command->line_number);
 			break;
 		case RANGE_FAIL:
-			printf("L%d: can't pchar, value out of range\n", command->line_number);
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", command->line_number);
 			break;
 		case INVAL_LINE:
-			printf("L%d: unknown instruction %s\n", command->line_number, info);
+			fprintf(stderr, "L%d: unknown instruction %s\n", command->line_number, info);
 			break;
 		case MALLOC_FAIL:
-			printf("Error: malloc failed\n");
+			fprintf(stderr, "Error: malloc failed\n");
 			break;
 		default:
 			printf("code works!\n");
